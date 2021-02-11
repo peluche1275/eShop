4135f3cf794a0ed6c64e686e436025a503a70d9d
Nathan <nathan.altomare.belier@gmail.com>
2021-02-11 10:29:49 +0100

    Some HTML

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
  
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-const index = require("./routes/index.js");
+const index = require("./routes/index.js")
 
-app.get('/', index)
+const listItem = [{
+    "item":"item 1",
+    "prix":"5€"
+},{
+    "item":"item 2",
+    "prix":"15€"
+}]
+
+app.get('/', function (req, res) {
+    res.render('template/template',{ viewPath:'../index',itemList:listItem })
+})
  
 app.listen(3000)
