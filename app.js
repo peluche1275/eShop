b598d788821c3d56317f2146a1d2d543fda28542
Nathan <nathan.altomare.belier@gmail.com>
2021-02-11 11:06:47 +0100

    Creation of the DataBase and first connection to

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express')
+const {MongoClient} = require('mongodb');
 const app = express()
+
+// Mongodb preparation
+const uri = "mongodb+srv://nathan:@nA1996plch@cluster0.sglyo.mongodb.net/test?retryWrites=true&w=majority"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function connectToDatabase(){
+    await client.connect();
+    const collectionItem = client.db("eshop").collection("item");
+    console.log("connecté à la database!")
+    console.log(await collectionItem.findOne({ id: "0" }))
+}
+
+connectToDatabase();
+
+//  ---------------
  
 app.set('view engine', 'ejs')
 
